55afad7286b4fed56ab1dfac9bb793665a22984a
Felix Reich <felix_reich@yahoo.com>
2026-09-25 12:34:14 +0200

    Update cg prediction strategy

--- linear_operator/utils/linear_cg.py
+++ linear_operator/utils/linear_cg.py
@@ -341,8 +341,6 @@ def linear_cg(
                 if not could_reorthogonalize:
                     save_directions_cg = False
                     num_stored = k
-                    tolerance_reached = True
-                    break
 
             d_mat[k].copy_(curr_conjugate_vec.squeeze(-1))
             kd_mat[k].copy_(mvms.squeeze(-1))
